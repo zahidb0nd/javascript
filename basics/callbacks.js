328cbad9e172a1b5b929abd04e446edcb185838e
zahidb0nd <zahidbond7@gmail.com>
2025-08-29 21:37:30 +0530

    callback

--- basics/callbacks.js
+++ basics/callbacks.js
@@ -1,15 +1,28 @@
-function hello(callback) {
-  console.log("hello");
-  callback();
+// function hello(callback) {
+//   console.log("hello");
+//   callback();
+// }
+// function wait() {
+//   console.log("wait!");
+// }
+
+// function leave() {
+//   console.log("please leave!");
+// }
+// function goodbye() {
+//   console.log("Good Bye!");
+// }
+// hello(wait);
+
+sum(displayPage, 3, 4);
+function sum(callback, x, y) {
+  let result = x + y;
+  callback(result);
 }
-function wait() {
-  console.log("wait!");
+function displayRes(result) {
+  console.log(result);
 }
 
-function leave() {
-  console.log("please leave!");
+function displayPage(result) {
+  document.getElementById("myh1").textContent = result;
 }
-function goodbye() {
-  console.log("Good Bye!");
-}
-hello(wait);
